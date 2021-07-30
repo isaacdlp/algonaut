e00310b3e5963dcd17907392e9aaa1cf5756789b
Isaac de la Peña <admin@agoraeafi.com>
2021-07-29 21:05:29 -0700

    ReCaptcha Added

--- assets/js/form.js
+++ assets/js/form.js
@@ -18,33 +18,38 @@ $(function() {
         // Stop the browser from submitting the form.
         e.preventDefault();
 
-        // Serialize the form data.
-        var formData = $(form).serialize();
+        grecaptcha.ready(function() {
+            grecaptcha.execute('reCAPTCHA_site_key', {action: 'submit'}).then(function(token) {
 
-        form_msg('bg-warn', 'Sending message. Just a second');
-        // Submit the form using AJAX.
-        $.ajax({
-            type: 'POST',
-            url: $(form).attr('action'),
-            data: formData
-        })
-            .done(function(response) {
-                if (response.status == "success") {
-                    // Make sure that the formMessages div has the 'success' class.
-                    form_msg('bg-success', 'Message sent. We will be in touch shortly');
-                } else {
-                    // Make sure that the formMessages div has the 'success' class.
-                    form_msg('bg-danger', 'Message not sent. Please try again');
-                }
+                // Serialize the form data.
+                var formData = $(form).serialize();
 
-                // Clear the form.
-                $('#name, #email, #message').val('');
-            })
-            .fail(function(data) {
-                // Make sure that the formMessages div has the 'error' class.
-                form_msg('bg-danger', 'Oops! An error occured and your message could not be sent.');
+                form_msg('bg-warn', 'Sending message. Just a second');
+                // Submit the form using AJAX.
+                $.ajax({
+                    type: 'POST',
+                    url: $(form).attr('action'),
+                    data: formData
+                })
+                    .done(function(response) {
+                        if (response.status == "success") {
+                            // Make sure that the formMessages div has the 'success' class.
+                            form_msg('bg-success', 'Message sent. We will be in touch shortly');
+                        } else {
+                            // Make sure that the formMessages div has the 'success' class.
+                            form_msg('bg-danger', 'Message not sent. Please try again');
+                        }
+
+                        // Clear the form.
+                        $('#name, #email, #message').val('');
+                    })
+                    .fail(function(data) {
+                        // Make sure that the formMessages div has the 'error' class.
+                        form_msg('bg-danger', 'Oops! An error occured and your message could not be sent.');
+                    });
+
             });
-
+        });
     });
 
 });
